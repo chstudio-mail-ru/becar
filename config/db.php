182c1531e8d799d42444175161f7303ad1d94a31
Denis Averkov <dmaverkov@gmail.com>
2020-12-25 02:16:15 +0300

    gii

--- config/db.php
+++ config/db.php
@@ -2,8 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;port=5433;dbname=becar',
     //'dsn' => 'pgsql:host=localhost;port=5432;dbname=becar',
-    'dsn' => 'pgsql:host=localhost;port=53738;dbname=becar',
+    //'dsn' => 'pgsql:host=localhost;port=53738;dbname=becar',
+    //'dsn' => 'pgsql:host=localhost;port=51193;dbname=becar',
     'username' => 'becar',
     'password' => 'admin',
     'charset' => 'utf8',
