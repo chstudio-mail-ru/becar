181780b2b680a9bd49bb6ea1e4131adbd3695a11
Denis A <you@example.com>
2020-12-25 18:59:12 +0300

    crud

--- config/db.php
+++ config/db.php
@@ -2,14 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5433;dbname=becar',
-    //'dsn' => 'pgsql:host=localhost;port=5432;dbname=becar',
-    //'dsn' => 'pgsql:host=localhost;port=53738;dbname=becar',
-    //'dsn' => 'pgsql:host=localhost;port=51193;dbname=becar',
-    'username' => 'becar',
+    //'dsn' => 'pgsql:host=localhost;port=5433;dbname=becar',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=becar',
+    'username' => 'postgres',
     'password' => 'admin',
     'charset' => 'utf8',
 
+    
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
